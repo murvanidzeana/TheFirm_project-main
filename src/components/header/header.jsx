28232e690c60c79dc76ca0378f3d6ebeae666274
@muru <118741835+murvanidzeana@users.noreply.github.com>
2023-05-10 00:16:02 +0400

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,27 @@
+import './header.css'
+import Logo from '../../pictures/logo.png'
+ 
+
+export default function Header() {
+    return(<>
+        <header>
+           
+           <img src={Logo} className="logo" alt=""/>
+        </header>
+        
+
+        <div>
+            <img src={Logo} className="logo" alt=""/>
+        </div>
+
+        <div>
+            <img src={Logo} className="logo" alt=""/>
+        </div>
+        <div>
+            <img src={Logo} className="logo" alt=""/>
+        </div>
+        </>
+        
+    )
+    
+}
