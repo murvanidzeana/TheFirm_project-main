deb08500c9005c2b2358543feaf11ebe7cca7161
@muru <118741835+murvanidzeana@users.noreply.github.com>
2023-05-10 16:35:13 +0400

    header4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 import Header from './components/header/header'
 import Firstconteiner from "./components/firstconteiner/firstconteiner";
 
-
 function App() {
     return(
     <div className="App">
     <Header/>
     <Firstconteiner/>
+    
 
     </div>
 
