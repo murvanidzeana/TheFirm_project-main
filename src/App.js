fba11f25b010d85add38ea94c151a7a1de456d6e
@muru <118741835+murvanidzeana@users.noreply.github.com>
2023-05-10 14:54:21 +0400

    header3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from './components/header/header'
+import Firstconteiner from "./components/firstconteiner/firstconteiner";
 
 
 function App() {
     return(
     <div className="App">
     <Header/>
+    <Firstconteiner/>
 
     </div>
 
---
+++ src/components/firstconteiner/firstconteiner.jsx
@@ -0,0 +1,24 @@
+import { isElementOfType } from 'react-dom/test-utils'
+import './firstconteiner.css'
+
+import Windows from '../../pictures/windowsIcon.png'
+import Google from '../../pictures/googleIcon.png'
+import Foxclon from '../../pictures/firefoxIcon.png'
+import Safari from '../../pictures/safari.png'
+export default function Firstconteiner(){
+    return(<>
+          <h2>Our Partner</h2>
+
+        <div className='Icons'>
+            <img src={Windows} className='' alt="" />
+            <img src={Google} className='' alt="" />
+            <img src={Foxclon} className='' alt="" />
+            <img src={Safari} className='' alt="" />
+
+
+
+
+        </div>
+        </>
+    )
+}
